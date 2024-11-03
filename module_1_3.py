5afa2bef94b9359b7426d23b877411f38ef480bf
anastdmr <nastyakokovina26@gmail.com>
2024-11-03 14:44:19 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Anastasia"
+print (name)
+age = 25
+print (age)
+age = age + 2
+print (age)
+is_student = True
+print (is_student)
---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+performed_work = 12
+spent_hours = 1.5
+course_title = "Python"
+#вычисление времени на одно задание
+time = spent_hours/performed_work
+print ("Курс: ",course_title,", всего задач: ",performed_work,", затрачено часов: ", spent_hours, ", среднее время выполнения: ", time, " часа.")
